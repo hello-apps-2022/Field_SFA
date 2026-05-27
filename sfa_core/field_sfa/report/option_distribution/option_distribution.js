d9cab946cd13b998f0300f308e558eec9a60cbe3
Tharun KC <tharunkc93@gmail.com>
2026-05-27 10:06:47 +0300

    Sync report JSON files updated by Frappe developer mode

---
+++ sfa_core/field_sfa/report/option_distribution/option_distribution.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Hema Beverages and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Option Distribution"] = {
+	"filters": [
+
+	]
+};
