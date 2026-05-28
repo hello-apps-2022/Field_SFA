8891b9b54c8488b52300642a4bdd8cf49df208c4
Tharun KC <tharunkc93@gmail.com>
2026-05-28 18:33:22 +0300

    Remove dead duplicate sfa_core/sfa_core tree

--- sfa_core/sfa_core/boot.py
+++
@@ -1,5 +0,0 @@
-import frappe
-
-def boot_session(bootinfo):
-    """Add SFA Core data to bootinfo"""
-    pass
--- sfa_core/sfa_core/page/sfa_form_templates/sfa_form_templates.js
+++
@@ -1,8 +0,0 @@
-frappe.pages['sfa-form-templates'].on_page_load = function(wrapper) {
-    frappe.set_route('sfa-dashboard');
-    setTimeout(function() {
-        if (window.SFA_Core && window.SFA_Core.router) {
-            window.SFA_Core.router.push('/form-templates');
-        }
-    }, 300);
-};
